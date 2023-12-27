6613822b1c2345030d01dfc69473b6a16f15536b
Alexey Boriskin <uruz@yandex-team.ru>
2023-12-27 23:55:18 +0400

    added

--- firstcondition/firstcond/firstcond/settings.py
+++ firstcondition/firstcond/firstcond/settings.py
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'firstcond.urls'
 
+#raise Exception(os.path.join(BASE_DIR, 'templates'))
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- firstcondition/firstcond/firstcond/views.py
+++ firstcondition/firstcond/firstcond/views.py
@@ -3,13 +3,30 @@ from django.views.generic import TemplateView
 
 
 class RussianView(TemplateView):
-    template_name = 'firtcond/russian.html'
+    template_name = 'russian.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RussianView, self).get_context_data(**kwargs)
+        context.update({
+            'lang': 'russian',
+            'title': 'Первое необходимое условие',
+        })
+        return context
+
 
 russian = RussianView.as_view()
 
 
 class EnglishView(TemplateView):
-    template_name = 'firstcond/english.html'
+    template_name = 'english.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EnglishView, self).get_context_data(**kwargs)
+        context.update({
+            'lang': 'english',
+            'title': 'First required condition',
+        })
+        return context
 
 
 english = EnglishView.as_view()
