c375c1201d90572c4a6d2543f479449de490d207
Alexey Boriskin <uruz@yandex-team.ru>
2023-12-29 00:41:11 +0400

    js + custom js

---
+++ firstcondition/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#run').click(function(){
+        $('#buttoncontainer').remove();
+        var replacement = '<iframe allow="autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr; cross-origin-isolated" id="game_drop" ' +
+            'src="/firstcondition/unity/2scenestest/index.html" mozallowfullscreen="true" allowtransparency="true" webkitallowfullscreen="true" frameborder="0" allowfullscreen="true";' +
+            'scrolling="no" msallowfullscreen="true" style="width: 100%; height: 100%;"></iframe>';
+        $('#game').html(replacement);
+    });
+});
