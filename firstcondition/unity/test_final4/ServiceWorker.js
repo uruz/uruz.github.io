c1e191a71401a9c51d8266c35ff45128cb39bb92
Alexey Boriskin <uruz@yandex-team.ru>
2023-12-30 01:13:07 +0400

    update unity

---
+++ firstcondition/unity/test_final4/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "BBB-v1-0.1";
+const contentToCache = [
+    "Build/test_final4.loader.js",
+    "Build/test_final4.framework.js",
+    "Build/test_final4.data",
+    "Build/test_final4.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
